61f45c931f70efc115b723d4813e1f5f7c2e11d9
nikhil <nikhilatoliya12@gmail.com>
2021-06-11 21:58:26 +0530

    first

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div>hello
+            </div>
+            <div className="left_header">heyyyyaa</div>
+            <div className="right_header">fucll</div>
+          
+        </div>
+
+
+    )
+}
+
+export default Header;
